2ce0d77adaad2e49c245da806d3621c3c7892fef
RepeatPan <Quit@users.noreply.github.com>
2014-03-25 06:54:36 +0100

    Added actions:think and actions:unthink
    Both take one argument, "thought", which is the path to the thought.

---
+++ ai/think.lua
@@ -0,0 +1,13 @@
+local Think = class()
+
+Think.name = "thinking"
+Think.does = "jelly:think"
+Think.args = { thought = "string" }
+Think.version = 2
+Think.priority = 1
+
+function Think:run(ai, entity, args)
+	radiant.entities.think(entity, args.thought)
+end
+
+return Think
