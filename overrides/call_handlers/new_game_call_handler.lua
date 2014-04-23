6b234ddec509f5189cd154ebf50011e19b7ed17e
RepeatPan <Quit@users.noreply.github.com>
2014-04-23 11:38:23 +0200

    Fixed new_game_call_handler.
    This commit also removes the "faction" entry from the data file. It was not much use to begin with...

--- overrides/call_handlers/new_game_call_handler.lua
+++ overrides/call_handlers/new_game_call_handler.lua
@@ -32,7 +32,6 @@ SOFTWARE.
 
 local MathFns = require("services.server.world_generation.math.math_fns")
 local BlueprintGenerator = require("services.server.world_generation.blueprint_generator")
-local game_master = require("services.server.game_master.game_master_service")
 local personality_service = stonehearth.personality
 local Point2 = _radiant.csg.Point2
 local Point3 = _radiant.csg.Point3
@@ -45,9 +44,19 @@ local NewGameCallHandler = class()
 function NewGameCallHandler:new_game(session, response, num_tiles_x, num_tiles_y, seed)
   local wgs = stonehearth.world_generation
   local blueprint
-  wgs:initialize(seed, true)
-  local generation_method = radiant.util.get_config("world_generation.method", "default")
+	local town = stonehearth.town:get_town(session.player_id)
   
+  if not town then
+    stonehearth.town:add_town(session)
+    stonehearth.inventory:add_inventory(session)
+    stonehearth.population:add_population(session)
+  end
+  
+  wgs:create_new_game(seed, true)
+	--[[ BEGIN JELLY ]]--
+	local generation_method = _host:get_config("mods.stonehearth.world_generation.method") or "default"
+	--[[ END JELLY ]]--
+	
   if generation_method == "tiny" then
     blueprint = wgs.blueprint_generator:get_empty_blueprint(2, 2)
   else
@@ -76,17 +85,19 @@ function NewGameCallHandler:generate_start_location(session, response, feature_c
   feature_cell_y = feature_cell_y + 1
   local wgs = stonehearth.world_generation
   local x, z = wgs.overview_map:get_coords_of_cell_center(feature_cell_x, feature_cell_y)
-  wgs.start_x = x
-  wgs.start_z = z
+	wgs.generation_location = Point3(x, 0, z)
   local radius = 2
   local blueprint = wgs:get_blueprint()
   local i, j = wgs:get_tile_index(x, z)
-  local generation_method = radiant.util.get_config("world_generation.method", "default")
+	--[[ BEGIN JELLY ]]--
+  local generation_method = _host:get_config("mods.stonehearth.world_generation.method") or "default"
   
-  if generation_method == "small" then
+	-- We assume that "tiny" should do both... otherwise you could have just one or another, err?
+  if generation_method == "small" or generation_method == "tiny" then
     radius = 1
   end
-  
+  --[[ END JELLY ]]--
+	
   if blueprint.width > 2 * radius + 1 then
     i = MathFns.bound(i, 1 + radius, blueprint.width - radius)
   end
@@ -102,8 +113,8 @@ end
 function NewGameCallHandler:embark_server(session, response)
   local scenario_service = stonehearth.scenario
   local wgs = stonehearth.world_generation
-  local x = wgs.start_x
-  local z = wgs.start_z
+  local x = wgs.generation_location.x
+  local z = wgs.generation_location.z
   local y = radiant.terrain.get_height(Point2(x, z))
   return {
     x = x,
@@ -119,13 +130,12 @@ function NewGameCallHandler:embark_client(session, response)
     local camera_service = stonehearth.camera
     local target = Point3f(o.x, o.y, o.z)
     local camera_location = Point3f(o.x, o.y + camera_height, o.z + target_distance)
-    camera_service._next_position = camera_location
-    camera_service:set_position(camera_location)
+		camera_service:set_position(camera_location, true)
     camera_service:look_at(target)
     _radiant.call("stonehearth:get_visibility_regions"):done(function(o)
       log:info("Visible region uri: %s", o.visible_region_uri)
       log:info("Explored region uri: %s", o.explored_region_uri)
-      _radiant.renderer.visibility.set_visibility_regions(o.visible_region_uri, o.explored_region_uri)
+      stonehearth.renderer:set_visibility_regions(o.visible_region_uri, o.explored_region_uri)
       response:resolve({})
     end)
   end)
@@ -137,33 +147,31 @@ function NewGameCallHandler:choose_camp_location(session, response)
   self._cursor_entity = radiant.entities.create_entity(json.banner_entity)
 	--[[ END JELLY ]]
   local re = _radiant.client.create_render_entity(1, self._cursor_entity)
-  self._capture = _radiant.client.capture_input()
-  self._capture:on_input(function(e)
-    
-    if e.type == _radiant.client.Input.MOUSE then
-      return self:_on_mouse_event(e.mouse, response)
-    elseif e.type == _radiant.client.Input.KEYBOARD then
-      return self:_on_keyboard_event(e.keyboard, response)
-    end
-    
-    return false
+  self._input_capture = stonehearth.input:capture_input():on_mouse_event(function(e)
+    return self:_on_mouse_event(e, response)
+  end):on_keyboard_event(function(e)
+    return self:_on_keyboard_event(e, response)
   end)
 end
 
 function NewGameCallHandler:_on_mouse_event(e, response)
-  assert(self._capture, "got mouse event after releasing capture")
+  assert(self._input_capture, "got mouse event after releasing capture")
   local s = _radiant.client.query_scene(e.x, e.y)
-  local pt = s.location and s.location or Point3(0, -100000, 0)
+  local pt = s:is_valid() and s:brick_of(0) or Point3(0, -100000, 0)
   pt.y = pt.y + 1
   self._cursor_entity:add_component("mob"):set_location_grid_aligned(pt)
   
-  if e:up(1) and s.location then
-    self._capture:destroy()
-    self._capture = nil
-    _radiant.call("stonehearth:create_camp", pt):always(function()
-      _radiant.client.destroy_authoring_entity(self._cursor_entity:get_id())
-      response:resolve({result = true})
-    end)
+  if e:up(1) and s:is_valid() then
+    self:_destroy_capture()
+    do
+      local default_camp_name = "Defaultville"
+      _radiant.call("stonehearth:create_camp", pt):done(function(o)
+        default_camp_name = o.random_town_name
+      end):always(function()
+        _radiant.client.destroy_authoring_entity(self._cursor_entity:get_id())
+        response:resolve({result = true, townName = default_camp_name})
+      end)
+    end
   end
   
   return true
@@ -180,77 +188,90 @@ function NewGameCallHandler:_on_keyboard_event(e, response)
 end
 
 function NewGameCallHandler:_destroy_capture()
-  self._capture:destroy()
-  self._capture = nil
+  if self._input_capture then
+    self._input_capture:destroy()
+    self._input_capture = nil
+  end
 end
 
 function NewGameCallHandler:create_camp(session, response, pt)
-  stonehearth.scenario:clear_starting_location(pt.x, pt.z)
+	stonehearth.world_generation:set_starting_location(Point2(pt.x, pt.z))
+	local town = stonehearth.town:get_town(session.player_id)
+	local pop = stonehearth.population:get_population(session.player_id)
+	local random_town_name = town:get_town_name()
 	--[[ BEGIN JELLY ]]--
 	-- Load the entities
 	local json = radiant.resources.load_json('jelly:index:camp_start')
-	local factionName = json.faction.name
 	
-  local faction = stonehearth.population:get_faction(factionName, json.faction.kingdom)
-  local town = stonehearth.town:get_town(session.faction)
   local location = Point3(pt.x, pt.y, pt.z)
   local banner_entity = radiant.entities.create_entity(json.banner_entity)
   radiant.terrain.place_entity(banner_entity, location)
   town:set_banner(banner_entity)
   local camp_x = pt.x
   local camp_z = pt.z
-	for _, workerDef in pairs(json.citizens) do
-		local worker = self:place_citizen(camp_x + workerDef.x, camp_z + workerDef.z)
-		radiant.events.trigger(personality_service, "stonehearth:journal_event", {
-			entity = worker,
+	
+	local function place_citizen_embark(x, z)
+		local citizen = self:place_citizen(pop, x, z)
+		radiant.events.trigger_async(personality_service, 'stonehearth:journal_event', {
+			entity = citizen,
 			description = "person_embarks"
 		})
 		
+		return citizen
+	end
+	
+	for _, workerDef in pairs(json.citizens) do
+		local worker = place_citizen_embark(camp_x + workerDef.x, camp_z + workerDef.z)
+
 		if workerDef.item then
-			radiant.entities.pickup_item(worker, faction:create_entity(workerDef.item))
+			radiant.entities.pickup_item(worker, pop:create_entity(workerDef.item))
 		end
 	end
 	
 	for _, entityDef in pairs(json.entities) do
-		self:place_item(entityDef.entity_ref, camp_x + entityDef.x, camp_z + entityDef.z, factionName)
+		self:place_item(pop, entityDef.entity_ref, camp_x + entityDef.x, camp_z + entityDef.z)
 	end
 	--[[ END JELLY ]]--
-  return {}
+  
+	return { random_town_name = random_town_name }
 end
 
-function NewGameCallHandler:place_citizen(x, z, profession)
-  local faction = stonehearth.population:get_faction("civ", "stonehearth:factions:ascendancy")
-  local citizen = faction:create_new_citizen()
-  profession = profession or "worker"
-  faction:promote_citizen(citizen, profession)
+function NewGameCallHandler:place_citizen(pop, x, z, profession)
+  local citizen = pop:create_new_citizen()
+  profession = profession or "stonehearth:professions:worker"
+  pop:promote_citizen(citizen, profession)
   radiant.terrain.place_entity(citizen, Point3(x, 1, z))
   return citizen
 end
 
-function NewGameCallHandler:place_item(uri, x, z, faction)
+function NewGameCallHandler:place_item(pop, uri, x, z)
   local entity = radiant.entities.create_entity(uri)
   radiant.terrain.place_entity(entity, Point3(x, 1, z))
-  
-  if faction then
-    entity:add_component("unit_info"):set_faction(faction)
-  end
-  
+  local unit_info = entity:add_component('unit_info')
+	unit_info:set_faction(pop:get_faction())
+	unit_info:set_player_id(pop:get_player_id())
   return entity
 end
 
-function NewGameCallHandler:place_stockpile(faction, x, z, w, h)
-  if not w or not w then
-    w = 3
-  end
+function NewGameCallHandler:get_town_name(session, response)
+  local town = stonehearth.town:get_town(session.player_id)
   
-  if not h or not h then
-    h = 3
-  end
+  if town then
+    return {
+      townName = town:get_town_name()
+    }
   
-  local location = Point3(x, 1, z)
-  local size = {w, h}
-  local inventory = stonehearth.inventory:get_inventory(faction)
-  inventory:create_stockpile(location, size)
+  else
+    return {
+      townName = "Defaultville"
+    }
+  end
+end
+
+function NewGameCallHandler:set_town_name(session, response, town_name)
+  local town = stonehearth.town:get_town(session.player_id)
+  town:set_town_name(town_name)
+  return true
 end
 
 return NewGameCallHandler
