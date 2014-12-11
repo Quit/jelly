9d59fc3ec57ca27a8bea29e4acccf320ba997f37
RepeatPan <Quit@users.noreply.github.com>
2014-12-11 18:08:58 +0100

    Restructured JS files; added two events for RootView in JS
    - `jelly.PreRootViewInit` is called before RootView.Init is called. Use this to modify Stonehearth views before they are used.
    - `jelly.PostRootViewInit` is called after RootView.Init was called.

---
+++ js/RootView.js
@@ -0,0 +1,10 @@
+// Overload App.RootView to add additional hooks
+App.RootView = App.RootView.extend({
+	init : function() {
+			var self = this;
+		
+			$(top).trigger('jelly.PreRootViewInit');
+			self._super();
+			$(top).trigger('jelly.PostRootViewInit');
+	}
+});
