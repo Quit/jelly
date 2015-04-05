e8726e0945284ff9bba77cc28d6da277da510807
RepeatPan <Quit@users.noreply.github.com>
2015-04-05 19:39:45 +0200

    Renamed events to be more properly named

--- js/RootView.js
+++ js/RootView.js
@@ -1,10 +1,14 @@
 // Overload App.RootView to add additional hooks
 App.RootView = App.RootView.extend({
    init : function() {
-         var self = this;
-      
-         $(top).trigger('jelly.PreRootViewInit');
-         self._super();
-         $(top).trigger('jelly.PostRootViewInit');
+      var self = this;
+
+      // DEPRECATED. Use jelly_pre_rootview_init instead.
+      $(top).trigger('jelly.PreRootViewInit');
+      $(top).trigger('jelly_pre_rootview_init', self);
+      self._super();
+      // DEPRECATED. Use jelly_post_rootview_init instead.
+      $(top).trigger('jelly.PostRootViewInit');
+      $(top).trigger('jelly_post_rootview_init', self);
    }
 });
